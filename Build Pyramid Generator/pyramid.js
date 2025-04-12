2e37f694ad5c3a7140c03a9791d7ef569c9a9a89
LouisVoterra <sunrisegamingiphone@gmail.com>
2025-04-12 12:51:24 +0700

    variables

--- Build Pyramid Generator/pyramid.js
+++ Build Pyramid Generator/pyramid.js
@@ -1,3 +1,6 @@
 console.log("hehe");
 
+// let character; //deklarasi variabel dengan judul character, dengan imbuhan kata depan let sebagai format penulisan javascript. Awalnta variabel ini tidak memiliki nilai
 
+//untuk menyimpan nilai di variabel character maka perlu menambahkan "assingment operator (=) ". Hal ini dilakukan agar dapat menyimpan value di dalam variabel character. Aksi ini disebut sebagai Initialization
+let character = "Hello"; 
